0cf521ea209464636dd931b238e85b397144b39e
raakesh-m <G17HUBG37B4Ndark$>
2024-09-05 12:09:49 +0530

    alpha version fix2

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    unoptimized: true, // Disable built-in image optimization
+  },
 }
 
 module.exports = nextConfig
